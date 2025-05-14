2d64f8af453cea5f58f89ad95f9320eee16c5854
kisrondox5@gmail.com <kisrondox5@gmail.com>
2025-05-14 08:20:29 +0200

    javítások

--- muzeum.js
+++ muzeum.js
@@ -1,23 +1,23 @@
 let sz=`
 <table>
-        <tr>
+        <tr style="text-align: center;">
           <td style="font-style: italic;">Athéni Régészeti Múzeum</td>
-          <td style=" width: 650px">Az Athéni Régészeti Múzeum Görögország legnagyobb és legjobb régészeti múzeuma, lenyűgöző gyűjteményével, amely több mint 11 000 kiállítást foglal magában. A régiségeknek ez a hatalmas skálája az őskortól a késő ókorig terjed. A kiállított tárgyak hatalmas száma ellenére a gondos és átgondolt elrendezésnek köszönhetően látogatásunk során soha nem érezte magát zsúfoltnak a múzeum. A múzeum egy nagyszerű, citromszínű neoklasszikus kastélyban található, és több helyiségre és melléképületre van felosztva. Szánjon időt arra, hogy megcsodálja az Őskori Régiségek Gyűjteményében található ókori eszközöket és leleteket, majd gyönyörködjön a görög szobrászat korszakokon átívelő fejlődésében a Szoborművek gyűjteményében.</td>
+          <td style="width:1300px;">Az Athéni Régészeti Múzeum Görögország legnagyobb és legjobb régészeti múzeuma, lenyűgöző gyűjteményével, amely több mint 11 000 kiállítást foglal magában. A régiségeknek ez a hatalmas skálája az őskortól a késő ókorig terjed. A kiállított tárgyak hatalmas száma ellenére a gondos és átgondolt elrendezésnek köszönhetően látogatásunk során soha nem érezte magát zsúfoltnak a múzeum. A múzeum egy nagyszerű, citromszínű neoklasszikus kastélyban található, és több helyiségre és melléképületre van felosztva. Szánjon időt arra, hogy megcsodálja az Őskori Régiségek Gyűjteményében található ókori eszközöket és leleteket, majd gyönyörködjön a görög szobrászat korszakokon átívelő fejlődésében a Szoborművek gyűjteményében.</td>
           <td><img style="height: 200px; width: 300px;" class="img-fluid" src="muzeum/1.jpg" alt=""></td>
         </tr>
-        <tr>
+        <tr style="text-align: center;">
           <td style="font-style: italic;">Bizánci és Keresztény Múzeum</td>
-          <td style=" width: 650px">A 2004-ben felújított csodálatos múzeum a bizánci művészet egyik legjobb gyűjteménye a világon. Az Ilissia villájában található ikonok, szobrok és freskók káprázatos skáláját mutatja be a keresztény kultúrából az i.sz. 3. századtól egészen a közelmúltig. A kiállítások szekciókba vannak rendezve és kategorizálva, hogy könnyen megtekinthetők legyenek.</td>
+          <td style="width:1300px;">A 2004-ben felújított csodálatos múzeum a bizánci művészet egyik legjobb gyűjteménye a világon. Az Ilissia villájában található ikonok, szobrok és freskók káprázatos skáláját mutatja be a keresztény kultúrából az i.sz. 3. századtól egészen a közelmúltig. A kiállítások szekciókba vannak rendezve és kategorizálva, hogy könnyen megtekinthetők legyenek.</td>
           <td><img style="height: 200px; width: 300px;" class="img-fluid" src="muzeum/2.jpg" alt=""></td>
         </tr>
-        <tr>
+        <tr style="text-align: center;">
             <td style="font-style: italic;">Országos Régészeti Múzeum</td>
-            <td style=" width: 650px">A Nemzeti Régészeti Múzeum egy kincsek múzeuma, amely a görögországi régészeti lelőhelyek leghíresebb leleteit mutatja be. A görög állam által gyűjtött műtárgyak jelentős tárháza is. A múzeumban hatalmas könyvtár, fényképészeti archívum és kémiai laboratóriumok találhatók.</td>
+            <td style="width:1300px;">A Nemzeti Régészeti Múzeum egy kincsek múzeuma, amely a görögországi régészeti lelőhelyek leghíresebb leleteit mutatja be. A görög állam által gyűjtött műtárgyak jelentős tárháza is. A múzeumban hatalmas könyvtár, fényképészeti archívum és kémiai laboratóriumok találhatók.</td>
             <td><img style="height: 200px; width: 300px;" class="img-fluid" src="muzeum/3.jpg" alt=""></td>
           </tr>
-          <tr>
+          <tr style="text-align: center;">
             <td style="font-style: italic;">Nemzeti Kortárs Művészeti Múzeum</td>
-            <td style=" width: 650px">A 2000-ben alapított Nemzeti Kortárs Művészeti Múzeum (más néven EMST) Athén egyik legfontosabb kulturális intézménye. Gyűjteményében görög és nemzetközi művészek festményei, installációi, fényképei és szobrai találhatók, mint például Vlassis Caniaris, Nikos Kessanlis, Sirin Nesat és Nan Goldin.</td>
+            <td style="width:1300px;">A 2000-ben alapított Nemzeti Kortárs Művészeti Múzeum (más néven EMST) Athén egyik legfontosabb kulturális intézménye. Gyűjteményében görög és nemzetközi művészek festményei, installációi, fényképei és szobrai találhatók, mint például Vlassis Caniaris, Nikos Kessanlis, Sirin Nesat és Nan Goldin.</td>
             <td><img style="height: 200px; width: 300px;" class="img-fluid" src="muzeum/4.jpg" alt=""></td>
           </tr>
       </table>
